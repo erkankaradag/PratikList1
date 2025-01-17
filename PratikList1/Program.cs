36e7e5475d1ea4c85ad81b507ff3f8a74eea01a8
erkankaradag <erkankaradagcom@gmail.com>
2025-01-17 14:08:38 +0300

    Add project files.

---
+++ PratikList1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PratikList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> davetliler = new List<string> { "Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+
+            foreach (string davetli in davetliler)
+
+            {
+                Console.WriteLine(davetli);
+            }
+        }
+    }
+}
